db2293b685d588ee19ae28d2a11f77edf8907c37
Tomas Chmelevskij <t.chmelevskij@gmail.com>
2022-04-01 13:44:17 +0200

    feat: add screenshots for testing

--- e2e/smoke.spec.js
+++ e2e/smoke.spec.js
@@ -5,11 +5,18 @@ const demos = await readdir('./demos');
 
 test.describe.parallel('Demos', () => {
   demos.forEach((demo) => {
-    test(`demo: ${demo}`, async ({ page }) => {
+    test(`demo: ${demo} should not crash`, async ({ page }) => {
       page.on('pageerror', (exception) => {
         expect(exception.message).toBe('');
       });
       await page.goto(`http://localhost:3333/demos/${demo}/`);
+    });
+
+    test(`demo: ${demo} should match screenshot`, async ({ page }) => {
+      await page.goto(`http://localhost:3333/demos/${demo}/`);
+      await page.waitForLoadState('networkidle')
+      await page.waitForTimeout(5000);
+      expect(await page.screenshot()).toMatchSnapshot({ maxDiffPixelRatio: demo === 'sample-tour' ? 0.2 : 0 });
     });
   });
 });
